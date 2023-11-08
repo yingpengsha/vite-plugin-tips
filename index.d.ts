f8b22d7ef179a4fe343e2ce030cd42330c971623
Pengsha Ying <yingpengsha@kuaishou.com>
2023-11-08 18:22:54 +0800

    chore: remove usless spaceline

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 import { Plugin } from 'vite'
 import { ViteTipsOptions as InnerViteTipsOptions } from './src/types'
 
-
 export type ViteTipsOptions = Partial<InnerViteTipsOptions>
 export function ViteTips(options?: ViteTipsOptions): Plugin
